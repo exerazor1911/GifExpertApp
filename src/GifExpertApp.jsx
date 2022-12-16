aa51ed6cdce71a4d4f24baf6dc39d10ebb12f640
exerazor1911 <exe_cien@hotmail.com>
2022-12-16 16:27:00 -0300

    agregados varios

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,9 +1,35 @@
-
+import { useState } from "react";
+import { AddCategory } from "./components/AddCategory";
+import { GifGrid } from "./components/GifGrid";
 
 export const GifExpertApp = () => {
+    const [categories, setCategories] = useState([ 'One Punch']);
+
+    console.log(categories);
+
+    const onAddCategory = (newCategory) => {
+      if (categories.includes(newCategory)) return;
+
+      setCategories([newCategory , ...categories]);
+    }
+
+
   return (
     <>
         <h1>GifExpertApp</h1>
+
+        <AddCategory 
+        // setCategories={setCategories}
+        onNewCategory={onAddCategory}
+        />
+
+        {/* <button onClick={onAddCategory}>Agregar</button> */}
+      
+            {categories.map(category => 
+                 (
+                <GifGrid key={category} category={category}/>
+                )
+            )}
     </>
   )
 }
---
+++ src/helpers/getGifs.js
@@ -0,0 +1,18 @@
+export const getGifs = async (category) => {
+
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=iILE0WT6MNYVGpucRODT5bSfS2n6MGTz&q=${category}&limit=20`
+    const resp = await fetch(url);
+    const {data} = await resp.json();
+
+    const gifs = data.map(img => (
+        {
+            id: img.id,
+            title: img.title,
+            url: img.images.downsized_medium.url
+        }
+    ))
+
+    console.log(gifs);
+    return gifs;
+
+}
